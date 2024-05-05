8f8b28b686e2d15f51da324314cf619ebf4ae3c7
”Vladislav <filatov_vn@magnit.ru>
2024-05-05 19:24:15 +0300

    task done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,49 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type Word struct {
+	Word  string
+	Count int
+}
+
+var sortedStruct []Word
+
+func Top10(input string) []string {
+	// 1. Отделить слова и превратить текст в слайс
+	dict := make(map[string]int)
+	words := regexp.MustCompile(`(?:\s-\s)|[,.\\s]+`).Split(input, -1)
+	wordsStr := strings.ToLower(strings.Join(words, " "))
+	// Избавляет от отступов, переноса строки, пробелов
+	words = strings.Fields(wordsStr)
+	// Благодаря свойству мап(неповторимость ключа)
+	// собираеми и считаем слова
+	for _, word := range words {
+		dict[word]++
+	}
+	// мапа не имеет функции сортировки - нужно преобразовать ее в слайс структур
+	for key, value := range dict {
+		sortedStruct = append(sortedStruct, Word{key, value})
+	}
+	// слайс структур теперь можно отсортировать по значению и лексикографически
+	sort.Slice(sortedStruct, func(i, j int) bool {
+		if sortedStruct[i].Count != sortedStruct[j].Count {
+			return sortedStruct[i].Count > sortedStruct[j].Count // по значению
+		}
+		return sortedStruct[i].Word < sortedStruct[j].Word // лексикографически
+	})
+	// слайс структур должен стать слайсом текстов - интересуют первые 10 слов
+	var result []string
+	for i, p := range sortedStruct {
+		if i < 10 {
+			result = append(result, p.Word)
+		} else {
+			break
+		}
+	}
+	return result
 }
