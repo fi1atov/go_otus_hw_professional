96fa57b29dcb461b6507bf894e0fa7ef5a28a790
”Vladislav <filatov_vn@magnit.ru>
2024-05-12 01:58:25 +0300

    Реализованы методы кэша. Имеющиеся тесты пройдены

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -7,36 +7,65 @@ import (
 type Key string
 
 // LRU - когда вытесняется элемент, к которому дольше всего не было обращений.
-type CacheMethods interface {
+type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
 	Clear()
 }
 
-type LruCache struct {
+type lruCache struct {
 	sync.Mutex
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
-func (c *LruCache) Set(key Key, value interface{}) {
+func (c *lruCache) Set(key Key, value interface{}) bool {
 	c.Lock()
 
 	defer c.Unlock()
 
-	c.items[key] = &ListItem{
-		Value: value,
-		Next:  nil,
-		Prev:  nil,
+	if _, found := c.items[key]; found { // если элемент найден в словаре
+		c.items[key].Value = value        // присвоили элементу новое значение
+		c.queue.MoveToFront(c.items[key]) // подвинули найденный элемент в начало списка
+		return true                       // элемент был
 	}
-
-	c.queue.PushFront(value)
+	// если элемент не найден в словаре - тогда это новый элемент который надо вставить
+	// если емкость достигла предела - то перед вставкой нового элемента - удалить самый старый
+	if c.queue.Len() >= c.capacity {
+		c.queue.Remove(c.queue.Back()) // получаем последний элемент из списка и удаляем его из списка
+		delete(c.items, key)           // после удаления из списка - удалить из мапы
+	}
+	item := c.queue.PushFront(value) // создать новый элемент в списке (и получить созданный элемент)
+	c.items[key] = item
+	return false // элемента не было
 }
 
-func NewCache(capacity int) LruCache {
-	return LruCache{
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.Lock()
+
+	defer c.Unlock()
+
+	if _, found := c.items[key]; found { // если элемент найден в словаре
+		c.queue.MoveToFront(c.items[key]) // подвинули найденный элемент в начало списка
+		return c.items[key].Value, true   // вернули значение элемента и статус что он найден
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.Lock()
+
+	defer c.Unlock()
+
+	for k := range c.items {
+		delete(c.items, k)
+	}
+}
+
+func NewCache(capacity int) Cache {
+	return &lruCache{
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
