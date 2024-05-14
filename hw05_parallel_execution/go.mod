ebc248b21e94afd6f4881376f4475d69c59f0750
”Vladislav <filatov_vn@magnit.ru>
2024-05-15 01:15:54 +0300

    Реализация запуска горутин

--- hw05_parallel_execution/go.mod
+++ hw05_parallel_execution/go.mod
@@ -1,6 +1,6 @@
-module github.com/fixme_my_friend/hw05_parallel_execution
+module github.com/fi1atov/go_otus_hw_professional/hw05_parallel_execution
 
-go 1.19
+go 1.22.1
 
 require (
 	github.com/stretchr/testify v1.7.0
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,6 +10,36 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	tasksChan := make(chan Task)
+	resultsChan := make(chan error, n-1)
+	closeChan := make(chan bool)
+	exitChan := make(chan bool)
+
+	// запускаются n горутин
+	for i := 0; i < n; i++ {
+		go startWorker(tasksChan, resultsChan, closeChan, exitChan)
+	}
 	return nil
+}
+
+func startWorker(tasksChan <-chan Task, resultsChan chan<- error, closeChan <-chan bool, exitChan chan<- bool) {
+	// Функция startWorker
+	// также отсылает сигнал в exitChan, когда завершает работу.
+	defer func() {
+		exitChan <- true
+	}()
+
+	// В каждой горутине происходит выбор из нескольких возможных операций
+	for {
+		select {
+		// Принятие задачи из tasksChan и выполнение её.
+		// Результат выполнения отправляется в resultsChan.
+		case task := <-tasksChan:
+			resultsChan <- task()
+		// Ожидание сигнала о закрытии (closeChan).
+		// Если такой сигнал приходит, горутина завершает свою работу.
+		case <-closeChan:
+			return
+		}
+	}
 }
