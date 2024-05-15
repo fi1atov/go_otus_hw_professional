4b981caf3c45fc2d0c2a583685dd6fb265c04d75
”Vladislav <filatov_vn@magnit.ru>
2024-05-15 18:37:21 +0300

    init

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,44 +2,81 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	tasksChan := make(chan Task)
-	resultsChan := make(chan error, n-1)
-	closeChan := make(chan bool)
-	exitChan := make(chan bool)
+// Run will run slice of functions on workersCount workers while get maxErrors errors.
+func Run(functions []Task, workersCount int, maxErrors int) error {
+	tasksChan := make(chan Task) // чан для всех задач
+	resultsChan := make(chan error, workersCount-1)
+	closeChan := make(chan bool) // сигнальный канал для закрытия
 
-	// запускаются n горутин
-	for i := 0; i < n; i++ {
-		go startWorker(tasksChan, resultsChan, closeChan, exitChan)
+	wg := sync.WaitGroup{}
+
+	// Запускаем наши горутины в числе workersCount
+	// они зависают на <-tasksChan - ждут пока там задачи появятся
+	for i := 0; i < workersCount; i++ {
+		wg.Add(1)
+		go startWorker(tasksChan, resultsChan, closeChan, &wg)
 	}
-	return nil
-}
 
-func startWorker(tasksChan <-chan Task, resultsChan chan<- error, closeChan <-chan bool, exitChan chan<- bool) {
-	// Функция startWorker
-	// также отсылает сигнал в exitChan, когда завершает работу.
-	defer func() {
-		exitChan <- true
+	// далее основная горутина должна закиндать в tasksChan задачи
+
+	counter, errorsSlice := func() (int, []error) {
+		var errors int
+		var inProgress int
+		var counter int
+		var errorsSlice []error
+
+		// отправка задач в канал - почему то отправятся не все задачи
+		fmt.Println("У нас всего заданий: - ", len(functions))
+		for i := 0; i < len(functions); i++ {
+			fmt.Println("Кладем задание в канал - ", i)
+			inProgress++
+			tasksChan <- functions[i]
+		}
+
+		for {
+			err := <-resultsChan
+			inProgress--
+			counter++ // счетчик исполненных заданий
+
+			if err != nil {
+				errorsSlice = append(errorsSlice, err)
+				errors++ // счетчик ошибок
+			}
+
+			if counter == len(functions) || errors == maxErrors {
+				close(closeChan)
+				return counter, errorsSlice
+			} else if len(functions)-counter-inProgress > 0 {
+				inProgress++
+				tasksChan <- functions[workersCount-1+counter]
+			}
+		}
 	}()
 
-	// В каждой горутине происходит выбор из нескольких возможных операций
+	wg.Wait()
+	fmt.Println(counter)
+	fmt.Println(errorsSlice)
+	return ErrErrorsLimitExceeded
+}
+
+func startWorker(tasksChan <-chan Task, resultsChan chan<- error, closeChan <-chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	for {
 		select {
-		// Принятие задачи из tasksChan и выполнение её.
-		// Результат выполнения отправляется в resultsChan.
+		// ждет пока задача появятся
 		case task := <-tasksChan:
-			resultsChan <- task()
-		// Ожидание сигнала о закрытии (closeChan).
-		// Если такой сигнал приходит, горутина завершает свою работу.
+			resultsChan <- task() // исполнение задачи
 		case <-closeChan:
-			return
+			return // канал закрылся - выходим из функции-исполнителя
 		}
 	}
 }
