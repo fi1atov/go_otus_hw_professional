b26f45b1f67b9c528ddb11c8e1795c3a36f212ed
”Vladislav <filatov_vn@magnit.ru>
2024-08-06 11:54:23 +0300

    work with params

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
+func getParams() (address string, timeout time.Duration) {
+	var host, port string
+	pflag.StringVarP(&host, "host", "h", "", "host")
+	pflag.StringVarP(&port, "port", "p", "", "port")
+	pflag.DurationVarP(&timeout, "timeout", "t", 10*time.Second, "connection timeout")
+
+	pflag.Parse()
+	if host == "" || port == "" {
+		log.Fatal("Please define address and port")
+	}
+	address = host + ":" + port
+	return
+}
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	address, timeout := getParams()
+	fmt.Println(address)
+	fmt.Println(timeout)
 }
