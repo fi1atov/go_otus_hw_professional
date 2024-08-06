6b598cb9e9261b84f8288f2189d0d739f617aacb
”Vladislav <filatov_vn@magnit.ru>
2024-08-06 18:59:34 +0300

    возвращаю струтуру клиента + основные методы интерфеса

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -13,9 +14,35 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+	closed  bool
+}
+
+func (c *client) Connect() error {
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *client) Close() error {
+	return nil
+}
+
+func (c *client) Send() error {
+	return nil
+}
+
+func (c *client) Receive() error {
+	return nil
+}
