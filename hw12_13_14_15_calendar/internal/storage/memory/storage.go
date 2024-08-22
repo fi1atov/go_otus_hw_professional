e5bf763bba41db1a52ad7d01b6772f89d7761f62
”Vladislav <filatov_vn@magnit.ru>
2024-08-22 14:33:14 +0300

    Добавлено описание сущностей

--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,14 +1,30 @@
 package memorystorage
 
-import "sync"
+import (
+	"context"
+	"github.com/fi1atov/go_otus_hw_professional/hw12_13_14_15_calendar/internal/storage"
+	"sync"
+)
+
+type data map[int]storage.Event
 
 type Storage struct {
 	// TODO
-	mu sync.RWMutex //nolint:unused
+	mu     sync.RWMutex //nolint:unused
+	lastID int
+	data   data
 }
 
-func New() *Storage {
-	return &Storage{}
+func New() storage.Storage {
+	result := Storage{}
+	result.data = make(data)
+	return &result
 }
 
-// TODO
+func (s *Storage) Connect(_ context.Context, _ string) error {
+	return nil
+}
+
+func (s *Storage) Close(_ context.Context) error {
+	return nil
+}
